813116191fc456e04f45470e64c52b6bec62fd82
Himanshu-Oli <68443232+Himanshu-Oli@users.noreply.github.com>
2022-07-12 08:33:32 +0530

    Update Main.java

--- Recursion/1. Fibonacci/Main.java
+++ Recursion/1. Fibonacci/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 	public static int fibo(int n) {
+		//Base case
 		if (n == 0) {
 			return 0;
 		}
 		if (n == 1) {
 			return 1;
 		}
+		// Recursive call + Small Calculation
 		return fibo(n-1) + fibo(n-2);
 	}
 	public static void main(String[] args) {

e6db14538fd327af3035dab2908a7fbead9f5a42
Himanshu-Oli <68443232+Himanshu-Oli@users.noreply.github.com>
2022-07-12 11:05:12 +0530

    Update Main.java

--- Recursion/2. Stairs(1 or 2)/Main.java
+++ Recursion/2. Stairs(1 or 2)/Main.java
@@ -1,11 +1,8 @@
 public class Main {
 	public static int stairs(int n) {
 		// Base case
-		if (n == 1) {
-			return 1;
-		}
-		if (n == 2) {
-			return 2;
+		if (n == 1 || n == 2) {
+			return n;
 		}
 		// Recursive call + Small calculation
 		return stairs(n-1) + stairs(n-2);
